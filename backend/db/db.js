501c6e97235a08651f8a5d11b4e40d99a16c14bd
Amir Raza <139304351+amirraja56@users.noreply.github.com>
2024-05-21 12:37:33 +0530

    Update db.js

--- backend/db/db.js
+++ backend/db/db.js
@@ -1,6 +1,6 @@
 const mongoose= require('mongoose');
 
-const url="mongodb://127.0.0.1/students";
+const url="mongodb+srv://amirraja56:raza@cluster0.vjl3pqj.mongodb.net/students?retryWrites=true&w=majority&appName=Cluster0";
 
 mongoose.connect(url).then(()=>{
     console.log('DataBase connected');
